84f2c05a0898575b4e8cbea1eb56ffac54654fa2
ayu <umaminokiami@gmail.com>
2024-08-21 04:20:10 +0900

    init

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from typing import List
+from src.WsManager import WsManager
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+manager = WsManager()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 特定のオリジンを許可する場合はここで指定します
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.websocket("/ws/")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received message: {data}") 
+            await manager.broadcast(f'{data}')
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
